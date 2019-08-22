5f7984c01bf3055c43ae38e96f35673ec6764ebc
mawen <mawen@meicloud.com>
2019-08-22 10:19:17 +0800

    latest code update on 2019.08.22 10:18

--- src/main/java/com/aboutThread/essential/DemoRunnable.java
+++ src/main/java/com/aboutThread/essential/DemoRunnable.java
@@ -6,20 +6,58 @@ import java.util.List;
 public class DemoRunnable implements Runnable {
     List<Integer> integerList = null;
     BigInteger total = BigInteger.ZERO;
-    public DemoRunnable(List<Integer> integerList){
+
+    public DemoRunnable(List<Integer> integerList) {
         this.integerList = integerList;
     }
 
-    public void run(){
-        synchronized (this){
-            for(Integer currentNum:integerList){
+    public void run() {
+        synchronized (this) {
+            final String threadName = Thread.currentThread().getName();
+
+            Thread countThread = new Thread() {
+                boolean flag = true;
+
+                public boolean isFlag() {
+                    return flag;
+                }
+
+                public void setFlag(boolean flag) {
+                    this.flag = flag;
+                }
+
+                @Override
+                public void run() {
+                    while (flag){
+                        int num = 1;
+                        for (; ; ) {
+                            System.out.println(threadName + " sleep " + num++ + "s");
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            };
+            try {
+                System.out.println(threadName + " waiting for 5s");
+                countThread.start();
+                this.wait(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.notify();
+
+            for (Integer currentNum : integerList) {
                 total = total.add(BigInteger.valueOf(currentNum));
             }
-            System.out.println("current thread name: " + Thread.currentThread().getName() + ", subNum: " + total);
+            System.out.println("current thread name: " + threadName + ", subNum: " + total);
         }
     }
 
-    public BigInteger getTotal(){
+    public BigInteger getTotal() {
         return total;
     }
 }
