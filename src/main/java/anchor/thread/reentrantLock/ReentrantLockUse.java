5cc959108eb1f8e3656f0b143429c46fbab0ffc5
mawen <mawen@meicloud.com>
2021-01-20 23:03:33 +0800

    complete ReentrantLockUse.java update readme.md

--- src/main/java/anchor/thread/reentrantLock/ReentrantLockUse.java
+++ src/main/java/anchor/thread/reentrantLock/ReentrantLockUse.java
@@ -6,14 +6,35 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Anchor
  */
 public class ReentrantLockUse {
-
+    private static int num = 0;
     final static ReentrantLock LOCK = new ReentrantLock();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        CustomThread t1 = new CustomThread("t1");
+        CustomThread t2 = new CustomThread("t2");
+        CustomThread t3 = new CustomThread("t3");
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println(num);
     }
 
-
+    /**
+     * 可重入，同一个线程可多次获得同一把锁
+     */
+    static void plus(){
+        LOCK.lock();
+        LOCK.lock();
+        try {
+            num++;
+        }finally {
+            LOCK.unlock();
+            LOCK.unlock();
+        }
+    }
 
     static class CustomThread extends Thread {
         public CustomThread(String name) {
@@ -23,13 +44,8 @@ public class ReentrantLockUse {
         @Override
         public void run() {
             System.out.println(this.getName() + " start running...");
-            LOCK.lock();
-            try {
-                for (int i = 0; i < 1000; i++) {
-
-                }
-            }finally {
-                LOCK.unlock();
+            for (int i = 0; i < 1000; i++) {
+                plus();
             }
         }
     }
