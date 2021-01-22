924e62d9186f2d25f288bdd78818b11928efdc16
Anchor <sweetsorrow211@gmail.com>
2021-01-22 22:47:31 +0800

    complete Interrupt.java

--- src/main/java/anchor/thread/reentrantLock/Interrupt.java
+++ src/main/java/anchor/thread/reentrantLock/Interrupt.java
@@ -9,8 +9,11 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Anchor
  *
  * 可中断锁是指：在等待获取锁的过程中可以被中断。
+ *
  * LOCK.lockInterruptibly() 与 LOCK.lock() 的作用相同
- * 不同的是
+ * 不同的是在获取锁的过程中，若线程调用了interrupt()方法，
+ * lockInterruptibly() 可以抛出 InterruptedException 异常，从而中断获取锁的过程，而 lock() 不能,
+ * 使用 try-catch 语句捕捉 lockInterruptibly() 抛出的异常可以自由进行其他操作
  */
 public class Interrupt {
 
@@ -23,12 +26,13 @@ public class Interrupt {
         CustomThread t2 = new CustomThread("t2", false);
         t1.start();
         t2.start();
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(3);
         t1.interrupt();
+        t2.interrupt();
     }
 
     static class CustomThread extends Thread {
-        private boolean flag;
+        private final boolean flag;
 
         public CustomThread(String name, boolean flag) {
             super(name);
@@ -41,15 +45,18 @@ public class Interrupt {
             try {
                 if (flag) {
                     LOCK1.lockInterruptibly();
+                    //睡眠三秒，给其他线程时间去获取另一把锁
+                    TimeUnit.SECONDS.sleep(3);
                     LOCK2.lockInterruptibly();
                 } else {
                     LOCK2.lockInterruptibly();
+                    TimeUnit.SECONDS.sleep(3);
                     LOCK1.lockInterruptibly();
                 }
-                TimeUnit.SECONDS.sleep(30);
+                TimeUnit.SECONDS.sleep(300);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                System.out.println("Catch InterruptedException,thread is interrupted: " + this.isInterrupted());
+                System.out.println("Catch InterruptedException," + this.getName() + " is interrupted: " + this.isInterrupted());
             } finally {
                 if (LOCK1.isHeldByCurrentThread()) {
                     LOCK1.unlock();
