a2203d032efc6e9503409adde578ee6b7a01e1ef
Anchor <sweetsorrow211@gmail.com>
2021-01-23 16:53:57 +0800

    complete sample ConditionUse.java and Await.java

---
+++ src/main/java/anchor/thread/condition/Await.java
@@ -0,0 +1,93 @@
+package anchor.thread.condition;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Anchor
+ *
+ * await的作用：释放锁并使线程进入等待状态。进入等待状态后可被 signal() 唤醒。
+ * 调用 await() 后有两种方式结束等待状态：1.被 signal() 2.线程被 interrupt()
+ *
+ * Condition 类中有五个 await 方法。抛出 InterruptedException 异常的方法等待状态都可以被 interrupt() 终结
+ * 1.void await() throws InterruptedException;
+ *   释放锁并使当前线程进入等待状态。可被 interrupt() 打断。
+ *
+ * 2.void awaitUninterruptibly();
+ *   释放锁并使当前线程进入等待状态。不可被 interrupt() 打断。
+ *
+ * 3.long awaitNanos(long nanosTimeout) throws InterruptedException;
+ *   释放锁并使当前线程进入等待状态。可被 interrupt() 打断。
+ *     a.nanosTimeout 表示指定的等待时间(单位为纳秒)，若指定时间到之后线程未被唤醒，则会自动醒来。
+ *     b.被唤醒时返回实际等待时间，自动唤醒时返回一个负数。
+ *
+ * 4.boolean await(long time, TimeUnit unit) throws InterruptedException;
+ *   释放锁并使当前线程进入等待状态。可被 interrupt() 打断。
+ *     a.time 表示指定的等待时间(可以更自由的指定时间单位)，若指定时间到之后线程未被唤醒，则会自动醒来。
+ *     b.被唤醒时返回 true，自动唤醒时返回 false。
+ *
+ * 5.boolean awaitUntil(Date deadline) throws InterruptedException;
+ *   释放锁并使当前线程进入等待状态。可被 interrupt() 打断。
+ *     a.deadline 表示一个时间点，若此时间点到来之后线程未被唤醒，则会自动醒来。
+ *     b.被唤醒时返回 true，自动唤醒时返回 false。
+ */
+public class Await {
+
+    final static ReentrantLock LOCK = new ReentrantLock();
+    final static Condition CONDITION1 = LOCK.newCondition();
+    final static Condition CONDITION2 = LOCK.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        T1 t1 = new T1();
+        T2 t2 = new T2();
+        t1.start();
+        TimeUnit.SECONDS.sleep(2);
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+    static class T1 extends Thread {
+        public T1() {
+            super("T1");
+        }
+
+        @Override
+        public void run() {
+            System.out.println(this.getName() + " start running...");
+            LOCK.lock();
+            try {
+                //被 signal() 了会返回 true，超过了等待时间返回 false
+                boolean await = CONDITION1.await(5, TimeUnit.SECONDS);
+                System.out.println("await: " + await);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                LOCK.unlock();
+            }
+        }
+    }
+
+    static class T2 extends Thread {
+        public T2() {
+            super("T2");
+        }
+
+        @Override
+        public void run() {
+            System.out.println(this.getName() + " start running...");
+            LOCK.lock();
+            try {
+                CONDITION1.signal();
+                //被 signal() 了会返回实际等待的时间，超过了等待时间返回一个负数
+                long await = CONDITION2.awaitNanos(TimeUnit.SECONDS.toNanos(5));
+                System.out.println("await: " + await);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                LOCK.unlock();
+            }
+        }
+    }
+}
--- src/main/java/anchor/thread/condition/ConditionUse.java
+++ src/main/java/anchor/thread/condition/ConditionUse.java
@@ -7,12 +7,16 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author Anchor
  *
- * Condition 可以代替 Object 中的 wait() notify() 实现等待/通知机制
- * Condition 由 ReentrantLock 对象调用 newCondition()创建
+ * 1.Condition 可以代替 Object 中的 wait() notify() 实现等待/通知机制
+ * 2.Condition 由 ReentrantLock 对象调用 newCondition()创建
+ * 3.调用 Condition 的方法前需先获取该对象关联的 ReentrantLock 锁
  */
 public class ConditionUse {
 
     final static ReentrantLock LOCK = new ReentrantLock();
+    /**
+     * 创建 Condition 对象
+     */
     final static Condition CONDITION = LOCK.newCondition();
 
     public static void main(String[] args) throws InterruptedException {
@@ -25,9 +29,7 @@ public class ConditionUse {
     }
 
     static class Wait extends Thread {
-        public Wait() {
-            super("Wait");
-        }
+        public Wait() {super("Wait");}
 
         @Override
         public void run() {
@@ -35,8 +37,10 @@ public class ConditionUse {
             LOCK.lock();
             try {
                 System.out.println(this.getName() + " start waiting...");
+                //作用同object.wait()，且会释放锁LOCK
                 CONDITION.await();
                 System.out.println(this.getName() + " resume...");
+                //调用wait()后，若等待过程中线程被interrupt()，wait()方法会抛出InterruptedException异常
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
@@ -47,9 +51,7 @@ public class ConditionUse {
     }
 
     static class Signal extends Thread {
-        public Signal() {
-            super("Signal");
-        }
+        public Signal() {super("Signal");}
 
         @Override
         public void run() {
@@ -57,6 +59,7 @@ public class ConditionUse {
             LOCK.lock();
             try {
                 System.out.println(this.getName() + " signal...");
+                //通知 Condition 结束等待，Condition 收到 signal() 后需要再次获取到锁才能继续执行
                 CONDITION.signal();
                 TimeUnit.SECONDS.sleep(3);
             } catch (Exception e) {
