71afeda5bfbc08a318b09ef6bcba0e11afef7999
Anchor <sweetsorrow211@gmail.com>
2021-01-25 10:40:02 +0800

    optimize MultiplePark.java

--- src/main/java/anchor/thread/lockSupport/MultiplePark.java
+++ src/main/java/anchor/thread/lockSupport/MultiplePark.java
@@ -16,25 +16,25 @@ import java.util.concurrent.locks.LockSupport;
  *    b.其他线程调用 interrupt() 方法打断了此线程
  *    c.park() 方法不合逻辑的 return 了(俗称见鬼了)
  *
- * 2.void parkNanos(long nanos)
+ * 2.void park(Object blocker)
+ *   同 1，参数 blocker 是为了方便 jstack 排查问题时当做标记使用，无其他用处
+ *   传一个类名比较好辨认的对象即可
+ *
+ * 3.void parkNanos(long nanos)
  *   同 1，但是增加了第四种情况：
  *    d.等待时间 nanos 到了
- *
- * 3.void park(Object blocker)
- *   同 2，参数 blocker 是为了方便 jstack 排查问题时当做标记使用，无其他用处
- *   传一个类名比较好辨认的对象即可
  *
  * 4.void parkNanos(Object blocker, long nanos)
  *   2、3 的结合体
  *
  * 5.void parkUntil(long deadline)
- *   同 2，但 deadline 是时间戳，而 2 中 nanos 是等待经过的时间。
+ *   同 3，但 deadline 是时间戳，而 2 中 nanos 是等待经过的时间。
  *
  * 6.void parkUntil(Object blocker, long deadline)
- *   3、5的结合体
+ *   2、5的结合体
  *
  * 总结：
- *    有两种方法可以手动结束线程等待状态：1.unpark();  2.interrupt();
+ *    有两种方法可以 手动 结束线程等待状态：1.unpark();  2.interrupt();
  */
 public class MultiplePark {
 
@@ -42,12 +42,14 @@ public class MultiplePark {
         System.out.println("pid: " + CommonUtil.getThreadPid());
         Thread t1 = new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + " start running...");
+            //等待 100 个小时
             LockSupport.parkNanos(new Logger1(), TimeUnit.HOURS.toNanos(100));
             System.out.println(Thread.currentThread().getName() + " is exiting...");
         });
 
         Thread t2 = new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + " start running...");
+            //等待 100 个小时
             LockSupport.parkUntil(new Logger2(), TimeUnit.HOURS.toNanos(100));
             System.out.println(Thread.currentThread().getName() + " is exiting...");
         });
@@ -56,6 +58,7 @@ public class MultiplePark {
         t1.start();
         t2.start();
         TimeUnit.SECONDS.sleep(3);
+        //虽然 t1 和 t2 都将等待100个小时，但是调用 interrupt() 之后会立即结束等待
         t1.interrupt();
         t2.interrupt();
     }
