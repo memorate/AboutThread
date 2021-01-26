c2402f9754daca57d79cd7d8534b1f849888c482
mawen <mawen@meicloud.com>
2021-01-26 22:49:17 +0800

    update BrokenBarrier.java

--- src/main/java/anchor/thread/juc/cyclicBarrier/BrokenBarrier.java
+++ src/main/java/anchor/thread/juc/cyclicBarrier/BrokenBarrier.java
@@ -13,14 +13,14 @@ import java.util.concurrent.TimeUnit;
  */
 public class BrokenBarrier {
 
-    private final static CyclicBarrier BARRIER = new CyclicBarrier(7);
+    private final static CyclicBarrier BARRIER = new CyclicBarrier(5);
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(CommonUtil.getThreadPid());
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 5; i++) {
             CustomThread thread = new CustomThread("t" + i);
             thread.start();
-            if (i == 4) {
+            if (i == 3) {
                 TimeUnit.SECONDS.sleep(1);
                 thread.interrupt();
                 TimeUnit.SECONDS.sleep(2);
@@ -35,11 +35,10 @@ public class BrokenBarrier {
             super(name);
         }
 
-        @Override
-        public void run() {
+        private void eat(){
             try {
                 System.out.println(this.getName() + " entering...");
-                if ("t4".equals(this.getName())) {
+                if ("t5".equals(this.getName())) {
                     TimeUnit.SECONDS.sleep(10);
                 }
                 BARRIER.await();
@@ -47,6 +46,25 @@ public class BrokenBarrier {
                 e.printStackTrace();
             }
             System.out.println(this.getName() + " finished eating...");
+        }
+
+        private void drive(){
+            try {
+                System.out.println(this.getName() + " leaving...");
+                if ("t5".equals(this.getName())) {
+                    TimeUnit.SECONDS.sleep(10);
+                }
+                BARRIER.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + " arrived...");
+        }
+
+        @Override
+        public void run() {
+            eat();
+            drive();
         }
     }
 }
